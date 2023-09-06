b0e08e8df3d452a84c38233d79a937c45335190f
nicholasbilotto <96328916+nicholasbilotto@users.noreply.github.com>
2023-09-06 16:58:45 -0400

    update build folder loc

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const hashAndSaveUsers = async () => {
 
 // Serve static files from the React frontend app
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../client/build")));
+	app.use(express.static(path.join(__dirname, "build")));
 	/* ROUTES */
 
 	app.use("/client", clientRoutes);
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV === "production") {
 
 	// Anything that doesn't match the above routes, send back the index.html file
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "../client/build/index.html"));
+		res.sendFile(path.join(__dirname, "build/index.html"));
 	});
 }
 
