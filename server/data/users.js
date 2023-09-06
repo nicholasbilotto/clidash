7043f97ef6b2ab132a9da7b572be003a4c3c1508
nicholasbilotto <96328916+nicholasbilotto@users.noreply.github.com>
2023-09-06 16:25:56 -0400

    updated folder organization

--- server/data/users.js
+++
@@ -1,6 +0,0 @@
-export const dataUsers = [
-	{ email: "nickbilotto@gmail.com", password: "T[}<J%HC(da&ZJ1" },
-	{ email: "pgerbino@cliemail.com", password: "*[BvcT@`10}2_1*" },
-	{ email: "daniel@cliemail.com", password: "pSI1p3J@VLDv+d." },
-	{ email: "gavin@totalwebpartners.com", password: "Z^7C.u5MiD:[=8l" },
-];
